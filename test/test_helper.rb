654327b8aa4c6abf1a3f9e732b9efe759722f7d4
nickbennett <nick.bennett@cutover.com>
2022-01-05 15:23:37 +0000

    Used layouts

---
+++ test/test_helper.rb
@@ -0,0 +1,12 @@
+ENV['RACK_ENV'] = 'test'
+
+require_relative '../app'
+require 'rack/test'
+require 'nokogiri'
+require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
+def doc(response)
+  Nokogiri::HTML(response.body)
+end
